c3fa62f1b5666fa3224334356ed1b1fbd2c34bb8
AlamSirji <alamsirji@pm.me>
2020-04-04 05:48:59 +0530

    Home

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Full Tutorial by AlamSirji</title>
+    <style media="screen">
+      * {
+        /* margin:0; */
+        /* padding:0; */
+        box-sizing: border-box;
+      }
+      body {
+        background-color: whitesmoke;
+      }
+      table {
+        border-collapse: collapse;
+        margin: 10px;
+      }
+      th,td {
+        border:1px solid;
+        padding:5px;
+        margin:5px;
+      }
+      footer {
+        text-align: center;
+        background-color: black;
+        color: white;
+        padding: 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>PHP Full Tutorial</h1>
+    <ul>
+      <li>PHP stands for <strong>PHP: Hypertext Preprocessor</strong>, earlier it was named as Personal Home Page</li>
+      <li>PHP is a an <strong>server-side scripting language</strong></li>
+      <li>PHP scripts are executed on the <strong>server</strong> and the result is returned to the browser as plain HTML</li>
+      <li>PHP is <strong>Free and open source</strong> </li>
+      <li>PHP files have extension <strong>.php</strong></li>
+      <li>PHP <strong>7</strong> is the stable release</li>
+      <li>PHP files can contain:
+        <ul>
+          <li>Text</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>PHP Code</li>
+        </ul>
+      </li>
+      <li>Official PHP Source: <a href="php.net">php.net</a></li>
+      <li>Unofficial PHP Source:
+        <ul>
+          <li><a href="https://www.w3schools.com/php/default.asp">W3Schools</a></li>
+        </ul>
+      </li>
+    </ul>
+    <h2>PHP Syllabus</h2>
+    <table>
+      <tr>
+        <th>Topic(s)</th>
+        <th>Description</th>
+        <th>Tutorial</th>
+      </tr>
+      <tr>
+        <td>Installation</td>
+        <td>Apache, PHP, MySQL</td>
+        <td><a href="dt1.php" target="_blank">Tutorial 1</a></td>
+      </tr>
+      <tr>
+        <td>Syntax</td>
+        <td>Normal & Shorthand</td>
+        <td><a href="dt2.php" target="_blank">Tutorial 2</a></td>
+      </tr>
+      <tr>
+        <td>Outputs</td>
+        <td>Echo & Print</td>
+        <td><a href="dt3.php" target="_blank">Tutorial 3</a></td>
+      </tr>
+      <tr>
+        <td>Comments</td>
+        <td>Single & Multi-line</td>
+        <td><a href="dt4.php" target="_blank">Tutorial 4</a></td>
+      </tr>
+    </table>
+    <footer>Designed and Developed by <a href="https://www.github.com/alamsirji" target="_blank">AlamSirji</a></footer>
+  </body>
+</html>
